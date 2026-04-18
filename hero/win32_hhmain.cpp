159a2de6aa8be2dabcbde310039b44de0749caaa
Karthik Rathinavelu <r.karthik@outlook.com>
2026-04-18 21:05:19 +0530

    Day 1 - Creating a simple msgbox and setting up the dev environment. No warnings during compilation

--- hero/win32_hhmain.cpp
+++ hero/win32_hhmain.cpp
@@ -1,10 +1,17 @@
 #include <windows.h>
 
-int WinMain(HINSTANCE hInstance, 
-			HINSTANCE hPrevInstance, 
-			LPSTR lpCmdLine, 
-			int nCmdShow)
+#pragma warning (disable:28251) // Disable warning about "Inconsistent annotation for 'WinMain' " because we don't want to annotate WinMain with SAL annotations.
+
+int APIENTRY
+WinMain(HINSTANCE hInstance, 
+		HINSTANCE, 
+		PSTR szCmdLine, 
+		int iCmdShow)
 {
-	MessageBoxA(NULL, "My Handmade Hero!", "Handmade Hero", MB_OK | MB_ICONINFORMATION);
+	UNREFERENCED_PARAMETER(hInstance);
+	UNREFERENCED_PARAMETER(szCmdLine);
+	UNREFERENCED_PARAMETER(iCmdShow);
+
+	MessageBox(NULL, TEXT("My Handmade Hero!"), TEXT("Handmade Hero"), MB_OK | MB_ICONINFORMATION);
 	return 0;
 }
